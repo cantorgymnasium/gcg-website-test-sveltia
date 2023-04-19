7e95d5443c1e1b0bc58fab027fde0b049e439413
Denys Konovalov <kontakt@denyskon.de>
2023-04-19 22:50:52 +0200

    fix date preview

--- static/admin/previews/field-previews/index.js
+++ static/admin/previews/field-previews/index.js
@@ -2,10 +2,12 @@ import BooleanPreview from "./boolean-preview.js";
 import DraftPreview from "./draft-preview.js";
 import CountPreview from "./count-preview.js";
 import BodyPreview from "./body-preview.js";
+import DatePreview from "./draft-preview.js";
 
 export {
 	BooleanPreview,
 	DraftPreview,
 	CountPreview,
 	BodyPreview,
+	DatePreview,
 };
