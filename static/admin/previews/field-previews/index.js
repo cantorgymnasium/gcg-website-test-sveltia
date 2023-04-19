403d0c1676646f92841edc5dc13896a4b4a3b472
Denys Konovalov <kontakt@denyskon.de>
2023-04-19 22:47:33 +0200

    fix redeclaration

--- static/admin/previews/field-previews/index.js
+++ static/admin/previews/field-previews/index.js
@@ -2,7 +2,6 @@ import BooleanPreview from "./boolean-preview.js";
 import DraftPreview from "./draft-preview.js";
 import CountPreview from "./count-preview.js";
 import BodyPreview from "./body-preview.js";
-import DraftPreview from "./draft-preview.js";
 
 export {
 	BooleanPreview,
