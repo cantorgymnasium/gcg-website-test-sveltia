16b7ce6f2faea293f7405eb72fcd509b962a18b8
Denys Konovalov <kontakt@denyskon.de>
2023-04-23 15:07:57 +0200

    „static/admin/previews/page-previews/styles.js“ ändern

--- static/admin/previews/page-previews/styles.js
+++ static/admin/previews/page-previews/styles.js
@@ -1,6 +1,7 @@
 const PreviewStyles = [
 	"https://assets.cantorgymnasium.de/bootstrap/v5/css/bootstrap.min.css",
 	"https://assets.cantorgymnasium.de/fonts/fira/fira.css",
+	"https://assets.cantorgymnasium.de/fonts/rubik/rubik.css",
 	"https://assets.cantorgymnasium.de/fonts/mdi/v7/css/materialdesignicons.min.css",
 	"https://cantorgymnasium.de/scss/style.css",
 ];
