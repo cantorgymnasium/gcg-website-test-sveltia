3259da24cdc4cd0242a6a557039fd10941cbe70a
Denys Konovalov <kontakt@denyskon.de>
2023-09-19 17:22:34 +0200

    static/admin/config/collections/index-pages.js aktualisiert

--- static/admin/config/collections/index-pages.js
+++ static/admin/config/collections/index-pages.js
@@ -214,6 +214,7 @@ const IndexPagesCollection = {
               hint: "Für mehrtägige Veanstaltungen, nur sichtbar wenn eingestellt",
               widget: "datetime",
               required: false,
+              default: "",
               ...DateFormat,
             },
             {
